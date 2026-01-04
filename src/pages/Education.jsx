1e92d1272ccb92bf547f7ea3225306f0d5dc4b80
Joe Meyer <joe@pocket-business.app>
2026-01-04 05:06:49 +0000

    File changes

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { 
   BookOpen, Search, GraduationCap, FileText, HelpCircle, BookMarked,
