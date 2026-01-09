d15dc4828c7a42b75f6b19b4075709ac19faa301
JoeMeyerSA <joe@pocket-business.app>
2026-01-09 18:12:20 +0200

    Standardise eligibility CTA label (About)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -229,7 +229,7 @@ export default function About() {
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button asChild size="lg" className="bg-[#0d9488] hover:bg-[#0f766e] text-white">
               <Link to={createPageUrl('EligibilityCheck')}>
-                Start Eligibility Check
+                Start eligibility check
                 <ArrowRight className="w-5 h-5 ml-2" />
               </Link>
             </Button>
